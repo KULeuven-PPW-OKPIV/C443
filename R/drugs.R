32c7a6cc081be65f7ec72dee44f17a2119e772d0
Aniek91 <aniek_sies@hotmail.com>
2023-06-20 18:41:05 +0200

    new url drugs data set

--- R/drugs.R
+++ R/drugs.R
@@ -2,7 +2,7 @@
 #'
 #' A dataset collected by Fehrman et al. (2017), freely available on the UCI Machine Learning Repository (Lichman, 2013) containing records of 1885 respondents regarding their use of 18 types of drugs, and their measurements on 12 predictors.
 #' #' All predictors were originally categorical and were quantified by Fehrman et al. (2017). The meaning of the values can be found on
-#' \url{https://archive.ics.uci.edu/ml/datasets/Drug+consumption+\%28quantified\%29}.
+#' \url{https://archive.ics.uci.edu/dataset/373/drug+consumption+quantified}.
 #' The original response categories for each drug were: never used the drug, used it over a decade ago, or in the last decade, year, month, week, or day.
 #' We transformed these into binary response categories, where 0 (non-user) consists of the categories never used the drug and used it over a decade ago and 1 (user) consists of all other categories.
 #' @format A data frame with 1185 rows and 32 variables:
@@ -41,7 +41,7 @@
 #'   \item{VSA}{volatile substance abuse user(1) or non-user (0)}
 #'
 #' }
-#' @source \url{https://archive.ics.uci.edu/ml/machine-learning-databases/00373/}
+#' @source \url{https://archive.ics.uci.edu/dataset/373/drug+consumption+quantified}
 #' @references \cite{Fehrman, E., Muhammad, A. K., Mirkes, E. M., Egan, V., & Gorban, A. N. (2017). The Five Factor Model of personality and evaluation of drug consumption risk. In Data Science (pp. 231-242). Springer, Cham.}
 #' \cite{Lichman, M. (2013). UCI machine learning repository.}
 "drugs"
